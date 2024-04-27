3011aee12c4db1f85f9ca5ebea5e614bf24b46e1
Neelam <pariharneelam376@gmail.com>
2024-04-27 23:57:10 +0530

    final change

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,7 @@ import Park from "./components/Park";
 import Boatclub from "./components/Boatclub";
 import Cafeteria from "./components/Cafeteria";
 import axios from "axios";
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
-axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 function App() {
   return (
     <>
